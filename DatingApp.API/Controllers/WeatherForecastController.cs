077b3098b7766a35284d20dedba83bd48529ac87
unknown <shaileshn011@gmail.com>
2019-09-27 15:34:48 +0530

    first commit

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {        
+        private readonly DataContext _context;
+        private readonly ILogger<WeatherForecastController> _logger;
+        public WeatherForecastController(DataContext context,ILogger<WeatherForecastController> logger)
+        {
+            _context = context;
+            _logger = logger;        
+        }
+        // private static readonly string[] Summaries = new[]
+        // {
+        //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        // };        
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {       
+            var Summaries= await _context.Values.ToListAsync(); 
+            var rng = new Random();
+            var data=  Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Count)].Name
+            })
+            .ToArray();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id){
+            var value=await _context.Values.SingleOrDefaultAsync(e=>e.Id==id);
+            return Ok(value);
+        }
+    }
+}
